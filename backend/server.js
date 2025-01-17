1c32b67df9536f37397a3428b16b7a547d574fd3
yoni-crypto <yonidisu111@gmail.com>
2025-01-17 17:59:12 +0300

    updated the serverjs to get the upload folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const authRoutes = require('./routes/authRoutes');
 const houseRoutes = require('./routes/houseRoutes');
