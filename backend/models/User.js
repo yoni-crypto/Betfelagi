5ca095d7577a4ac78522b11068b09de190956217
yoni-crypto <yonidisu111@gmail.com>
2025-01-17 16:53:09 +0300

    updated bcrypt with bcryptjs

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
