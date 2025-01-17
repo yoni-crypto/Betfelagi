8b820ade2797714667aba648d4964a5c1826ec1c
yoni-crypto <yonidisu111@gmail.com>
2025-01-18 01:24:28 +0300

    fixed my backend to return user house

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,7 +23,25 @@ const getUserProfile = async (req, res) => {
         res.status(500).json({ message: 'Failed to fetch user profile', error });
     }
 };
+const getUserHouses = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const houses = await House.find({ user: userId });
+
+        if (!houses) {
+            return res.status(404).json({ message: 'No houses found for this user' });
+        }
+
+        res.status(200).json(houses);
+    } catch (error) {
+        console.error('Error fetching houses:', error);
+        res.status(500).json({ message: 'Failed to fetch houses', error });
+    }
+};
+
 
 module.exports = {
     getUserProfile,
+    getUserHouses,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getUserProfile } = require('../controllers/userController');
+const { getUserProfile, getUserHouses } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 
 router.get('/profile', protect, getUserProfile);
+router.get('/:userId/houses',  getUserHouses);
 
 module.exports = router;
